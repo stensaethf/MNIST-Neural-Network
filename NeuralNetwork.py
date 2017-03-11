2cf17ba00a0b577abddb7204f5584121b56fd899
Frederik Stensaeth <stensaethf@carleton.edu>
2017-03-11 11:15:12 -0600

    added NeuralNetwork.py

---
+++ NeuralNetwork.py
@@ -0,0 +1,19 @@
+# NeuralNetwork
+# Neural network implementation.
+# 03.11.17
+
+class NeuralNetwork:
+	def __init__(self):
+		# do stuff
+
+		# filler, remove later
+		x = 1
+
+def main():
+	# do stuff
+
+	# filler, remove later
+	print "inside"
+
+if __name__ == '__main__':
+	main()
