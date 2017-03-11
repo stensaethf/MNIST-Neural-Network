60431a90247a3672a19f0e0ec4a56f4c86fd9c73
Frederik Stensaeth <stensaethf@carleton.edu>
2017-03-11 17:11:02 -0600

    bugs

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -7,8 +7,8 @@ import loadData, numpy as np, random
 class NeuralNetwork:
 	def __init__(self):
 		# initialize the neural network as a dict of dicts of weights
-		# self.layerlens = [784, 10]
-		self.layers = [1, 1, 1]
+		self.layers = [784, 10]
+		# self.layers = [1, 1, 1]
 		self.weights = dict()
 		# bias[layer][node]
 		self.bias = dict()
@@ -25,17 +25,19 @@ class NeuralNetwork:
 
 	def feedForward(self, result):
 		# weights: [layer][node]
+		output = [result]
+		ins = []
 		for layer in self.weights:
-			output = []
-			ins = []
+			output.append([])
+			ins.append([])
 			for node_index in range(len(self.weights[layer])):
 				dot_product = np.dot(self.weights[layer][node_index], result)
 				bias = self.bias[layer][node_index]
-				ins.append(dot_product + bias)
-				output.append(self.sigma(dot_product + bias))
-			result = output
+				ins[-1].append(dot_product + bias)
+				output[-1].append(self.sigma(dot_product + bias))
+			result = output[-1]
 
-		return result, ins
+		return output, ins
 
 	def backpropogate(self, examples, labels):
 		# compute the Del values for output units using observed error (eq. 18.8)
@@ -58,6 +60,7 @@ class NeuralNetwork:
 				y = labels[i]
 
 				# feed forward
+				# a, ins = [[]]
 				a, ins = self.feedForward(x)
 
 				Del = []
@@ -66,21 +69,44 @@ class NeuralNetwork:
 
 				# start by calculating the Dels
 				for j in range(self.layers[-1]):
-					Del[j] = self.sigmaPrime(ins[j])*(y[j]-a[j])
+					Del.append(self.sigmaPrime(ins[-1][j])*(y-a[-1][j]))
 
 				# propagate back through the rest of the layers
 				for l in range(len(self.layers)-1, 0, -1):
-					for j in self.weights[l]:
-						Del[j] = self.sigmaPrime(ins[j])*sum(self.weights[l][m][j]*Del[m] for m in range(self.layers[l-1]))
+					for j in range(len(self.weights[l])):
+						Del[j] = self.sigmaPrime(ins[l-1][j])*sum(self.weights[l][j][m]*Del[j] for m in range(self.layers[l-1]))
 						#Del[j] = self.sigmaPrime(ins[j])*np.dot(self.weights[l][j], Del)
 
 				# update every weight in network using dels
-				for l in range(len(self.layers)):
+				for l in range(1, len(self.layers)):
 					for j in range(self.layers[l]):
-						for m in self.weights[l][j]:
-							self.weights[l][j][m] = self.weights[l][j][m] + self.alpha*a[m]*Del[j]
+						for m in range(len(self.weights[l][j])):
+							# print "l-1"
+							# print a[m]
+							# print a[l-1]
+							# print m
+							# print a[l-1][m]
+							# print "DEL"
+							# print Del[j]
+							self.weights[l][j][m] = self.weights[l][j][m] + self.alpha*a[l-1][m]*Del[j]
+
+			self.numberCorrect(examples, labels)
 
 		#learning rate: \alpha(t)=1000/(1000+t) seems good
+
+	def numberCorrect(self, images, labels):
+		count = 0
+		for i in range(len(images)):
+			image = images[i]
+			label = labels[i]
+			# feed forward
+			a, ins = self.feedForward(image)
+			index = a.index(max(a))
+
+			if index == label:
+				count += 1
+
+		print str(count) + "/" + str(len(images))
 
 	def sigma(self, x):
 		# return 1.0 / (1 + Math.exp(-x))
@@ -92,18 +118,15 @@ class NeuralNetwork:
 def main():
 	# # Loads the train, dev and test sets.
 	# # 50,000, 10,000, 10,000
-	# train, dev, test = loadData.loadMNIST()
-	# # Gets the training images.
-	# train_images = train[0]
-	# # Gets the training labels.
-	# train_labels = train[1]
+	train, dev, test = loadData.loadMNIST()
+	# Gets the training images.
+	train_images = train[0]
+	# Gets the training labels.
+	train_labels = train[1]
 
-	# print len(train_images)
-	# print len(train_labels)
-
-	net = NeuralNetwork()
-	result = net.feedForward([0])
-	print result
+	network = NeuralNetwork()
+	network.backpropogate(train_images, train_labels)
+	network.numberCorrect(dev[0], dev[1])
 
 if __name__ == '__main__':
 	main()
