9169e3fcb99070a126ef317cce181704714837bb
Frederik Stensaeth <stensaethf@carleton.edu>
2017-03-12 16:50:42 -0500

    fixed the bug!

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -23,18 +23,16 @@ class NeuralNetwork:
 		# 	jth neuron in the second layer.
 		for i in range(1, len(self.layers)):
 			self.weights.append(np.zeros((self.layers[i], self.layers[i-1])))
-			# self.bias.append(np.zeros(self.layers[i]))
-			self.bias.append(np.zeros((self.layers[i])))
+			self.bias.append(np.zeros((self.layers[i], 1)))
 			for j in range(self.layers[i]):
-				self.bias[-1][j] = random.random()/100
+				self.bias[-1][j][0] = random.random()/100
 				for k in range(self.layers[i - 1]):
-					# self.bias[-1][j][k] = random.random()/100
 					self.weights[-1][j][k] = random.random()/100
 
 	def feedForward(self, result):
 		thresholds = []
 		acts = [result]
-		for layer in range(len(self.layers)-1):
+		for layer in range(len(self.weights)):
 			weights = self.weights[layer]
 			bias = self.bias[layer]
 			threshold = np.dot(weights, result) + bias
@@ -43,7 +41,7 @@ class NeuralNetwork:
 			acts.append(result)
 		return thresholds, acts
 
-	def backpropogate(self, examples, labels):
+	def backpropogate(self, example, label):
 		# compute the Del values for output units using observed error (eq. 18.8)
 		# starting at output layer, repeat for each hidden layer until earliest reached
 			# propagate the Del values back to previous layer
@@ -53,73 +51,90 @@ class NeuralNetwork:
 		# 18.8: w_i=w_i+\alpha(y-h_w(x))*h_w(x)(1-h_w(x))*x_i
 		# h_w(x)=Log(w*x)=1/(1+e^{-w*x})  ---Threshold function---
 
+		x = example
+		y = label
+
+		# feed forward
+		thresholds, acts = self.feedForward(x)
+
+		# propagate deltas backward from output layer to input layer
+
+		# start by calculating the deltas of the output layer.
+		# len(self.layers)-1 gives us the last layer
+		delta = (
+			self.error(y, acts[-1]) *
+			self.sigmaPrime(thresholds[-1])
+		)
+
+		# Propagate back through the rest of the layers.
+
+		# first layer does not have weights, so actually only
+		# len(self.layers)-1 with weights.
+		# len(self.layers)-2 would give us the correct indexing.
+		# however, we want to skip the last layer, so
+		# len(self.layers)-2 to 1 (we write 0 because it doesn't execute it).
+
+		# each bias and weight has a change associated with it, so
+		# let's create matrices of the same structure as we already have.
+		weights_change = [np.zeros(weights.shape) for weights in self.weights]
+		bias_change = [np.zeros(bias.shape) for bias in self.bias]
+
+		# the change made to the weights are the dot product of the
+		# delta for that layer and the activations of the previous
+		# layer.
+		# activations for biases are always 1, so the change needed
+		# is just the delta.
+		weights_change[-1] = np.dot(delta, acts[-2].transpose())
+		bias_change[-1] = delta
+
+		# now we want to find the changes needed to be made to the
+		# rest of the weights and biases.
+		for l in range(2, len(self.layers)):
+			delta = (
+				np.dot(self.weights[-l+1].transpose(), delta) *
+				self.sigmaPrime(thresholds[-l])
+			)
+
+			# once again, the bias change is just the delta, as acts
+			# would be all 1.
+			bias_change[-l] = delta
+			weights_change[-l] = np.dot(delta, acts[-l-1].transpose())
+
+		# update every weight in network using deltas.
+		for l in range(len(self.layers)-1):
+			self.weights[l] = (
+				self.weights[l] + 
+				self.alpha * weights_change[l]
+			)
+			self.bias[l] = (
+				self.bias[l] + 
+				self.alpha * bias_change[l]
+			)
+
+	def train(self, examples, labels):
 		for t in range(self.iterations): #repeat some number of times
 			# self.alpha = (1000/1000+t)
 			self.alpha = 0.3
+
+			# shuffle the examples and labels to that we do not train on
+			# them in the same order every iteration.
+			examples_new = []
+			labels_new = []
+			index_shuf = range(len(examples))
+			random.shuffle(index_shuf)
+			for i in index_shuf:
+				examples_new.append(examples[i])
+				labels_new.append(labels[i])
+
+			examples = examples_new
+			labels = labels_new
+
 			for e in range(len(examples)):
-				x = examples[e]
-				y = np.zeros(self.layers[-1])
-				y[labels[e]] = 1
+				x = np.reshape(examples[e], (784, 1))
+				y = np.zeros((self.layers[-1], 1))
+				y[labels[e]] = 1.0
 
-				# feed forward
-				thresholds, acts = self.feedForward(x)
-
-				# propagate deltas backward from output layer to input layer
-
-				# start by calculating the deltas of the output layer.
-				# len(self.layers)-1 gives us the last layer
-				delta = (
-					self.sigmaPrime(thresholds[-1]) *
-					self.error(y, acts[-1])
-				)
-
-				# Propagate back through the rest of the layers.
-
-				# first layer does not have weights, so actually only
-				# len(self.layers)-1 with weights.
-				# len(self.layers)-2 would give us the correct indexing.
-				# however, we want to skip the last layer, so
-				# len(self.layers)-2 to 1 (we write 0 because it doesn't execute it).
-
-				# each bias and weight has a change associated with it, so
-				# let's create matrices of the same structure as we already have.
-				weights_change = [np.zeros(weights.shape) for weights in self.weights]
-				bias_change = [np.zeros(bias.shape) for bias in self.bias]
-
-				# the change made to the weights are the dot product of the
-				# delta for that layer and the activations of the previous
-				# layer.
-				# activations for biases are always 1, so the change needed
-				# is just the delta.
-				# acts[-2]
-				weights_change[-1] = np.dot(delta, acts[-2].transpose())
-				bias_change[-1] = delta
-
-				# now we want to find the changes needed to be made to the
-				# rest of the weights and biases.
-				# want: delta_i = sigmaPrime(threshold_i) * dot(weight_i, delta_j)
-				# where delta_j is the next layer.
-				for l in range(2, len(self.layers)):
-					delta = (
-						self.sigmaPrime(thresholds[-l]) *
-						np.dot(self.weights[-l+1].transpose(), delta)
-					)
-
-					# once again, the bias change is just the delta, as acts
-					# would be all 1.
-					bias_change[-l] = delta
-					weights_change[-l] = np.dot(delta, acts[-l].transpose())
-
-				# update every weight in network using deltas.
-				for l in range(len(self.layers)-1):
-					self.weights[l] = (
-						self.weights[l] + 
-						self.alpha * weights_change[l]
-					)
-					self.bias[l] = (
-						self.bias[l] + 
-						self.alpha * bias_change[l]
-					)
+				self.backpropogate(x, y)
 
 			self.numberCorrect(examples, labels)
 
@@ -129,8 +144,9 @@ class NeuralNetwork:
 	def numberCorrect(self, images, labels):
 		count = 0
 		for i in range(len(images)):
-			image = images[i]
+			image = np.reshape(images[i], (784, 1))
 			label = labels[i]
+
 			# feed forward
 			thresholds, acts = self.feedForward(image)
 			index = np.argmax(acts[-1])
@@ -141,7 +157,6 @@ class NeuralNetwork:
 		print str(count) + "/" + str(len(images))
 
 	def sigma(self, x):
-		# return 1.0 / (1 + Math.exp(-x))
 		return 1.0 / (1 + np.exp(-x))
 
 	def sigmaPrime(self, x):
@@ -156,9 +171,10 @@ def main():
 	# Gets the training labels.
 	train_labels = train[1]
 
-	network = NeuralNetwork(5, [784, 10, 10])
-	network.backpropogate(train_images, train_labels)
-	# network.numberCorrect(dev[0][:1000], dev[1][:1000])
+	network = NeuralNetwork(5, [784, 30, 10])
+	network.train(train_images, train_labels)
+	# try it on the dev set.
+	network.numberCorrect(dev[0], dev[1])
 
 if __name__ == '__main__':
 	main()
